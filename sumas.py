efe686aa49ac748a3a24d09e4cd078b87ecabefe
Mafepe16 <marcosfernandezperegrina16@gmail.com>
2026-08-09 18:01:51 +0200

    Programa para sumar

---
+++ sumas.py
@@ -0,0 +1,6 @@
+numero1 = int(input("Pinkfloyd, inserta el primer número: "))
+numero2 = int(input("Verdulero de mierda, dae el segundo número: "))
+
+resultado = numero1 + numero2
+
+print("Tu resultado es: ", resultado)
